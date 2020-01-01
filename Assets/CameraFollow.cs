c46c35f09a1461e8f74eacfb0de13148307047ff
danjinng <dndanielng929@gmail.com>
2020-01-01 17:11:46 +0800

    imported free 0x72 dungoen sprites camera follow between player and crosshair player movement

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform player_transform;
+    private Camera cam;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private float smoothTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_transform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = new Vector3(player_transform.position.x, player_transform.position.y, transform.position.z);
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 crosshairPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 CamPos = player_transform.position + (crosshairPos - player_transform.position) / 5;
+
+        Vector3 camTarget = new Vector3(CamPos.x, CamPos.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, camTarget, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+    private float moveVector_x;
+    private float moveVector_y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement inputs
+        moveVector_x = Input.GetAxis("Horizontal");
+        moveVector_y = Input.GetAxis("Vertical");
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //movement
+        rb.velocity = new Vector2(moveVector_x, moveVector_y) * moveSpeed * Time.deltaTime;
+    }
+}
