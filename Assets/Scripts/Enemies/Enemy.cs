a6ae277df2175594086a69acc4ffc32346a49abd
danjinng <dndanielng929@gmail.com>
2020-01-13 17:59:16 +0800

    room generator able to generated an empty room with walls surrounding, collisions work with pathfinding

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public abstract class Enemy : MonoBehaviour, IDamageable
+{
+    protected float health;
+    protected float maxHeath;
+
+    //empty game object with a transform for where this enemy will move
+    [SerializeField] protected GameObject transformToSetAsPatrolArea;
+    [SerializeField] protected float patrolRange;
+
+    protected ObjectPooler objectPooler;
+    protected PlayerController playerController;
+
+    protected IAstarAI ai;
+    protected AIDestinationSetter aiDestinationSetter;
+    protected AstarPath aStarPath;
+    protected GridGraph gridGraph;
+
+    protected virtual void Awake()
+    {
+        objectPooler = GameManager.instance.GetComponent<ObjectPooler>();
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    protected virtual void Move()
+    {
+        
+    }
+
+    protected virtual void Patrol()
+    {
+        //while
+               // aiDestinationSetter.targetPosition = 
+    }
+
+    protected virtual void SetNewPatrolDestination()
+    {
+        //while (gridGraph.)
+        {
+            Vector2 tempDestination = (Vector2)transformToSetAsPatrolArea.transform.position + new Vector2(Random.Range(-patrolRange, patrolRange), Random.Range(-patrolRange, patrolRange));
+
+        }
+
+    }
+
+    protected abstract void Attack();
+
+    void IDamageable.DamagedByAmount(float damage)
+    {
+        health -= damage;
+    }
+
+    protected virtual void Die()
+    {
+        //TODO: animation or timer for fade
+        objectPooler.DespawnObjectIntoPool(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Pathfinding;
+
+public class RoomGenerator : MonoBehaviour
+{
+    [SerializeField] private Tilemap floorTilemap;
+    [SerializeField] private Tilemap wallTilemap;
+
+    [SerializeField] private List<Tile> floorTiles;
+    [SerializeField] private List<Tile> wallTiles;
+    //private List<>
+
+    public IAstarAI Ai;
+    private AstarPath activeAstarPath;
+
+    public Vector3Int Center;
+    public int roomWidth;
+    public int roomHeight;
+
+    private void Awake()
+    {
+        activeAstarPath = AstarPath.active;
+        PlaceRoom(Center);
+    }
+
+    private void Start()
+    {
+
+
+
+    }
+
+    private void Update()
+    {
+        //scan the graph the frame immediately after start frame (i.e. second frame of the scene loading)
+        if (Time.time == Time.deltaTime) activeAstarPath.Scan();
+
+
+    }
+
+    public void PlaceRoom(Vector3Int center)
+    {
+        PlaceGroundTiles(center);
+        PlaceWallTiles(center);
+    }
+
+    public void PlaceGroundTiles(Vector3Int center)
+    {
+        for (int x = 0; x < roomWidth; x++)
+        {
+            for (int y = 0; y < roomHeight; y++)
+            {
+                int randInt = Random.Range(0, floorTiles.Count);
+                floorTilemap.SetTile(center + new Vector3Int(roomWidth / 2 - x, roomHeight / 2 - y, 0), floorTiles[randInt]);
+            }
+        }
+    }
+
+    public void PlaceWallTiles(Vector3Int center)
+    {
+        for (int x = -1; x < roomWidth + 1; x++)
+        {
+            int randInt = Random.Range(0, wallTiles.Count);
+            wallTilemap.SetTile(center + new Vector3Int(roomWidth / 2 - x, roomHeight / 2 + 1, 0), wallTiles[randInt]);
+            randInt = Random.Range(0, wallTiles.Count);
+            wallTilemap.SetTile(center + new Vector3Int(roomWidth / 2 - x, roomHeight / 2 - roomHeight, 0), wallTiles[randInt]);
+        }
+
+        for (int y = 0; y < roomHeight; y++)
+        {
+            int randInt = Random.Range(0, wallTiles.Count);
+            wallTilemap.SetTile(center + new Vector3Int(roomWidth / 2 + 1, roomHeight / 2 - y, 0), wallTiles[randInt]);
+            randInt = Random.Range(0, wallTiles.Count);
+            wallTilemap.SetTile(center + new Vector3Int(roomWidth / 2 - roomWidth, roomHeight / 2 - y, 0), wallTiles[randInt]);
+        }
+    }
+}
